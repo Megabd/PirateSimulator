ec5cfd32dae85e800254e95fe05cf00819b64fe8
Lignio <oliverflycktw@gmail.com>
2025-11-06 12:05:08 +0100

    added ecs stuff wip

---
+++ Assets/HealthSystem.cs
@@ -0,0 +1,26 @@
+using Unity.Burst;
+using Unity.Entities;
+
+partial struct HealthSystem : ISystem
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach (var HealthComponent in SystemAPI.Query<RefRW<HealthComponent>>())
+        {
+
+        }
+    }
+
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    {
+        
+    }
+}
---
+++ Assets/SpeedComponent.cs
@@ -0,0 +1,9 @@
+using Unity.Entities;
+
+public struct SpeedComponent : IComponentData
+{
+    float turnSpeed; // deg/sec
+    public float Speed;
+}
+
+
